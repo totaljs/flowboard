659016d62fbfd3aa757bec3e5e7151ff418b9076
Martin Smola <smola.martin@gmail.com>
2017-10-22 11:00:30 +0200

    Updated dropdown component

--- source/js/ui.js
+++ source/js/ui.js
@@ -1969,7 +1969,7 @@ COMPONENT('dropdown', function(self, config) {
 
 				self.bind('', items);
 				break;
-			case 'condition':
+			case 'if':
 				condition = value ? FN(value) : null;
 				break;
 			case 'required':
@@ -2043,6 +2043,7 @@ COMPONENT('dropdown', function(self, config) {
 		content = self.html();
 		self.aclass('ui-dropdown-container');
 		self.redraw();
+		config.if && (condition = FN(config.if));
 		config.items && self.reconfigure({ items: config.items });
 		config.datasource && self.reconfigure('datasource:' + config.datasource);
 	};
